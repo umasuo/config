5b26b49518ddaeddadab7dc9fdcf542fafd1d46a
umasuo <liuquan89@gmail.com>
2017-08-28 15:11:41 +0800

    refactor code

--- src/main/java/com/umasuo/config/Application.java
+++ src/main/java/com/umasuo/config/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
- * Created by umasuo on 17/2/7.
+ * Config Application.
  */
 @SpringBootApplication
 @EnableConfigServer
